e9fa1caa5fadeaead40d14f3ad6efea7c6204090
Alex Kramer <kramer.alex.kramer@gmail.com>
2012-07-10 03:43:47 -0400

    Execute user code in separate temp directories

--- contrib/ipython-testing/forking_kernel_manager.py
+++ contrib/ipython-testing/forking_kernel_manager.py
@@ -3,6 +3,8 @@ import os
 import signal
 import sys
 import resource
+import tempfile
+import shutil
 from IPython.zmq.ipkernel import IPKernelApp
 from IPython.config.loader import Config
 from multiprocessing import Process, Pipe
@@ -23,12 +25,20 @@ class ForkingKernelManager(object):
             self.update_function(ka)
         for r, limit in resource_limits.iteritems():
             resource.setrlimit(getattr(resource, r), (limit, limit))
-        pipe.send({"ip": ka.ip, "key": ka.session.key, "shell_port": ka.shell_port,
-                "stdin_port": ka.stdin_port, "hb_port": ka.hb_port, "iopub_port": ka.iopub_port})
+        pipe.send({"ip": ka.ip,
+                   "key": ka.session.key,
+                   "shell_port": ka.shell_port,
+                   "stdin_port": ka.stdin_port,
+                   "hb_port": ka.hb_port,
+                   "iopub_port": ka.iopub_port})
         pipe.close()
         ka.start()
 
     def start_kernel(self, kernel_id=None, config=None, resource_limits=None, logfile = None):
+        current_dir = os.getcwd()
+        tmp_dir = tempfile.mkdtemp()
+        os.chdir(tmp_dir)
+
         if kernel_id is None:
             kernel_id = str(uuid.uuid4())
         if config is None:
@@ -40,7 +50,13 @@ class ForkingKernelManager(object):
         proc.start()
         connection = p.recv()
         p.close()
-        self.kernels[kernel_id] = (proc, connection)
+
+        os.chdir(current_dir)
+
+        self.kernels[kernel_id] = {"process": proc,
+                                   "connection": connection,
+                                   "tmp_dir": tmp_dir,
+                                   }
         return {"kernel_id": kernel_id, "connection": connection}
 
     def kill_kernel(self, kernel_id):
@@ -48,7 +64,7 @@ class ForkingKernelManager(object):
         success = False
 
         if kernel_id in self.kernels:
-            proc = self.kernels[kernel_id][0]
+            proc = self.kernels[kernel_id]["process"]
             try:
                 success = True
                 os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
@@ -59,6 +75,7 @@ class ForkingKernelManager(object):
                 from errno import ESRCH
                 if e.errno !=  ESRCH:
                     success = False
+        shutil.rmtree(self.kernels[kernel_id]["tmp_dir"])
         if success:
             del self.kernels[kernel_id]
         return success
@@ -69,7 +86,7 @@ class ForkingKernelManager(object):
 
         if kernel_id in self.kernels:
             try:
-                os.kill(self.kernels[kernel_id][0].pid, signal.SIGINT)
+                os.kill(self.kernels[kernel_id]["process"].pid, signal.SIGINT)
                 success = True
             except:
                 pass
@@ -77,9 +94,9 @@ class ForkingKernelManager(object):
         return success
 
     def restart_kernel(self, kernel_id):
-        ports = self.kernels[kernel_id][1]
+        connection = self.kernels[kernel_id]["connection"]
         self.kill_kernel(kernel_id)
-        return self.start_kernel(kernel_id, Config({"IPKernelApp": ports}))
+        return self.start_kernel(kernel_id, Config({"IPKernelApp": connection}))
 
 if __name__ == "__main__":
     def f(a,b,c,d):
